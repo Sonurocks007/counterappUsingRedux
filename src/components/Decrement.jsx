5c65840fafa7cb9f1e70772ac724e9c700c02606
Sonubgupta.hp@gmail.com <Sonubgupta.hp@gmail.com>
2025-07-30 11:45:03 +0530

    Decrement feature added in Counter component

---
+++ src/components/Decrement.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { decrement } from '../redux/CounterSlice';
+
+
+const Decrement = () => {
+    const dispatch = useDispatch();
+    return (
+    <button 
+    onClick={()=>dispatch(decrement())}
+    className="bg-red-500 hover:bg-red-600 text-white px-5 py-2 rounded-md shadow-md transition duration-300">
+    Decrement
+    </button>
+  )
+}
+
+export default Decrement
