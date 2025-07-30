22e99469f02b4be31b558486e145eb3398d9d037
Sonubgupta.hp@gmail.com <Sonubgupta.hp@gmail.com>
2025-07-30 18:01:31 +0530

    added todo slice with feature add ,delete and edit

---
+++ src/redux/slices/TodoSlice.jsx
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const TodoSlice = createSlice({
+  name: 'todos',
+  initialState: [],
+  reducers: {
+    addTodo: (state, action) => {
+      state.push({ id: Date.now(), text: action.payload });
+    },
+    deleteTodo: (state, action) => {
+      return state.filter((todo) => todo.id !== action.payload);
+    },
+    editTodo: (state, action) => {
+      const { id, newText } = action.payload;
+      const todo = state.find((todo) => todo.id === id);
+      if (todo) {
+        todo.text = newText;
+      }
+    },
+  },
+});
+
+export const { addTodo, deleteTodo, editTodo } = TodoSlice.actions;
+export default TodoSlice.reducer;
